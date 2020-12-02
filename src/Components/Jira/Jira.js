ae45ef65ddf60f538e8f3d6354dbbe4c0db29f61
Ryan Meyer <ryan@atriadev.com>
2020-12-02 14:18:42 -0700

    seperate project from projectlist, added cors url

--- src/Components/Jira/Jira.js
+++ src/Components/Jira/Jira.js
@@ -1,11 +1,12 @@
 import Axios from 'axios';
 
+const cors = 'https://cors-anywhere.herokuapp.com/'
 const baseUrl = 'https://atriadev.atlassian.net/rest/api/2';
 const username = 'ryan@atriadev.com';
 const apiKey = 'LgwiZqAfFk3b5bz07X3998AB';
 
 const jira = Axios.create({ 
-    baseURL: baseUrl,
+    baseURL: cors + baseUrl,
     auth: {
         username: username,
         password: apiKey
---
+++ src/Components/Project/Project.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Project (props) {
+    let projectProgress = '50%';
+    const avatar = "project.lead.avatarUrls.16x16";
+    return (
+        props.projects.map(project => (
+            <tr key={project.key}>
+              <td className="project-status"><span className="label label-primary">Active</span></td>
+              <td className="project-title"><Link to={`/issues/${project.key}`}>{project.name}</Link></td>
+              <td className="project-completion">
+                <small>Completion with : {projectProgress}</small>
+                <div className="progress progress-mini">
+                <div className="progress-bar" style={{width: projectProgress}}></div>
+                </div>
+              </td>
+              <td className="project-people">
+                <img alt="Ryan Meyer" src={avatar} className="rounded-circle"  />
+                </td>
+              <td className="project-actions">
+              <button href="#" className="btn btn-white btn-sm"><i className="fa fa-folder"></i> View </button>
+              <button href="#" className="btn btn-white btn-sm"><i className="fa fa-pencil"></i> Edit </button>
+              </td>
+              </tr>
+          ))
+          
+    )
+}
+
+export default Project;
